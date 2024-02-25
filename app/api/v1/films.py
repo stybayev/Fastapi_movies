5b9fbb87e60cada02cc5e880f16c65f336ec086d
dosxana <dosxana@gmail.com>
2024-02-25 23:24:08 +0600

    Implementation of the full information on the film

--- app/api/v1/films.py
+++ app/api/v1/films.py
@@ -1,32 +1,94 @@
 from http import HTTPStatus
-
 from fastapi import APIRouter, Depends, HTTPException
-from pydantic import BaseModel
-
 from app.services.film import FilmService, get_film_service
 
 router = APIRouter()
 
+from pydantic import BaseModel
+from typing import List, Optional
 
-class Film(BaseModel):
-    id: str
+
+class BasePersonModelResponse(BaseModel):
+    """
+    Базовая модель персоны для ответа API
+    """
+    uuid: str
+    full_name: str
+
+
+class GenreResponse(BaseModel):
+    """
+    Базовая модель для жанров ответа API
+    """
+    uuid: str
+    name: str
+
+
+class DirectorResponse(BasePersonModelResponse):
+    """
+    Модель режиссёра для ответа API
+    """
+    pass
+
+
+class ActorResponse(BasePersonModelResponse):
+    """
+    Модель актера для ответа API
+    """
+    pass
+
+
+class WriterResponse(BasePersonModelResponse):
+    """
+    Модель сценариста для ответа API
+    """
+    pass
+
+
+class FilmResponse(BaseModel):
+    uuid: str
     title: str
+    description: Optional[str] = None
+    imdb_rating: float
+    genre: List[GenreResponse]
+    directors: List[DirectorResponse]
+    actors: List[ActorResponse]
+    writers: List[WriterResponse]
 
 
 # Внедряем FilmService с помощью Depends(get_film_service)
-@router.get('/{film_id}', response_model=Film)
-async def film_details(film_id: str, film_service: FilmService = Depends(get_film_service)) -> Film:
+@router.get('/{film_id}', response_model=FilmResponse)
+async def film_details(
+        film_id: str,
+        film_service: FilmService = Depends(get_film_service)) -> FilmResponse:
+    """
+    Получить информацию о фильме
+    :param film_id:
+    :param film_service:
+    :return:
+    """
     film = await film_service.get_by_id(film_id)
     if not film:
-        # Если фильм не найден, отдаём 404 статус
-        # Желательно пользоваться уже определёнными HTTP-статусами, которые содержат enum
-        # Такой код будет более поддерживаемым
-        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='film not found')
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND,
+                            detail='film not found')
 
-    # Перекладываем данные из models.Film в Film
-    # Обратите внимание, что у модели бизнес-логики есть поле description
-    # Которое отсутствует в модели ответа API.
-    # Если бы использовалась общая модель для бизнес-логики и формирования ответов API
-    # вы бы предоставляли клиентам данные, которые им не нужны
-    # и, возможно, данные, которые опасно возвращать
-    return Film(id=film.id, title=film.title)
+    # Преобразование данных об актёрах, сценаристах, режиссорах
+    actors_response = [ActorResponse(uuid=actor.id, full_name=actor.name) for actor in film.actors]
+    writers_response = [WriterResponse(uuid=writer.id, full_name=writer.name) for writer in film.writers]
+    directors_response = [DirectorResponse(
+        uuid=director.uuid,
+        full_name=director.full_name) for director in film.director]
+
+    # Создание и возврат объекта ответа, используя преобразованные данные
+    return FilmResponse(
+        uuid=film.id,
+        title=film.title,
+        description=film.description,
+        imdb_rating=film.imdb_rating,
+        genre=film.genre,
+        directors=directors_response,
+        actors_names=film.actors_names,
+        writers_names=film.writers_names,
+        actors=actors_response,
+        writers=writers_response
+    )
--- app/models/film.py
+++ app/models/film.py
@@ -1,7 +1,43 @@
-from pydantic import Field
-from typing import List
-from app.models.persons import Actor, Writer
+from pydantic import Field, BaseModel as BaseModelFromPydantic
+from typing import List, Optional
 from app.models.base_model import BaseMixin
+
+
+class BasePersonModel(BaseMixin):
+    """
+    Базовая модель персоны
+    """
+    name: str
+
+
+class Genre(BaseModelFromPydantic):
+    """
+    Модель жанра, связанная с фильмом
+    """
+    uuid: str
+    name: str
+
+
+class Director(BaseModelFromPydantic):
+    """
+    Модель режисреа, связанная с фильмом
+    """
+    uuid: str
+    full_name: str
+
+
+class Actor(BasePersonModel):
+    """
+    Модель актера, связанная с фильмом
+    """
+    pass
+
+
+class Writer(BasePersonModel):
+    """
+    Модель сценариста, связанная с фильмом
+    """
+    pass
 
 
 class Film(BaseMixin):
@@ -9,10 +45,10 @@ class Film(BaseMixin):
     Модель фильма
     """
     title: str
-    description: str
+    description: Optional[str] = None
     imdb_rating: float
-    genre: List[str]
-    director: List[str] = Field(default_factory=list)
+    genre: List[Genre] = Field(default_factory=list)
+    director: List[Director] = Field(default_factory=list)
     actors_names: List[str] = Field(default_factory=list)
     writers_names: List[str] = Field(default_factory=list)
     actors: List[Actor] = Field(default_factory=list)
