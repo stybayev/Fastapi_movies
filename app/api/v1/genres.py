3569fc8e9e990f948b4e16990d77cb97e8ce7841
Kirill <spore-pirat@mail.ru>
2024-02-26 22:06:13 +0300

    added paginator

--- app/api/v1/genres.py
+++ app/api/v1/genres.py
@@ -3,6 +3,7 @@ from typing import List
 
 from app.db.elastic import get_elastic, AsyncElasticsearch
 from app.models.genre import Genre
+from app.utils.paginator import paginate
 
 router = APIRouter()
 
@@ -13,6 +14,6 @@ async def get_genre_by_id(genre_id: str, es: AsyncElasticsearch = Depends(get_el
     return Genre(**response['_source'])
 
 @router.get("/", response_model=List[Genre])
-async def genre(es: AsyncElasticsearch = Depends(get_elastic)) -> List[Genre]:
-    response = await es.search(index="genres")
+async def genre(es: AsyncElasticsearch = Depends(get_elastic), page_params: dict = Depends(paginate)) -> List[Genre]:
+    response = await es.search(index="genres", **page_params)
     return [Genre(**item['_source']) for item in response['hits']['hits']]
