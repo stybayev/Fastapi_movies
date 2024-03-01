5b34a40185e2e4c774450a5e5f3ab7725797da2e
Kirill <spore-pirat@mail.ru>
2024-03-01 22:38:29 +0300

    added lifespan instead of event handlers

--- app/main.py
+++ app/main.py
@@ -5,32 +5,33 @@ from elasticsearch import AsyncElasticsearch
 from fastapi.responses import ORJSONResponse
 from fastapi import FastAPI
 from redis.asyncio import Redis
+from contextlib import asynccontextmanager
 
 from app.api.v1 import films, genres, persons
 from app.core import config
 from app.core.logger import LOGGING
 from app.db import elastic, redis
 
-app = FastAPI(
-    title=config.PROJECT_NAME,
-    docs_url="/api/openapi",
-    openapi_url="/api/openapi.json",
-    default_response_class=ORJSONResponse,
-)
 
-
-@app.on_event("startup")
-async def startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     redis.redis = Redis(host=config.REDIS_HOST, port=config.REDIS_PORT)
     elastic.es = AsyncElasticsearch(
         hosts=[f'http://{config.ELASTIC_HOST}:{config.ELASTIC_PORT}']
     )
 
+    yield
 
-@app.on_event("shutdown")
-async def shutdown():
     await redis.redis.close()
     await elastic.es.close()
+
+app = FastAPI(
+    title=config.PROJECT_NAME,
+    docs_url="/api/openapi",
+    openapi_url="/api/openapi.json",
+    default_response_class=ORJSONResponse,
+    lifespan=lifespan
+)
 
 
 app.include_router(films.router, prefix="/api/v1/films", tags=["films"])
